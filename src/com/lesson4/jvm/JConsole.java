9accddc72f45acdd148638561c52e75e2e9ea9a4
yqs <411012285@qq.com>
2018-01-17 17:09:34 +0800

    master

---
+++ src/com/lesson4/jvm/JConsole.java
@@ -0,0 +1,26 @@
+package com.lesson4.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yqs on 2018/1/17.
+ */
+public class JConsole {
+    static class OOMObject{
+        public  byte[] placeholder = new byte[64 *1024];
+    }
+
+    public static void fillHeap(int num) throws InterruptedException {
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        for (int i =0;i<num;i++){
+            Thread.sleep(10);
+            list.add(new OOMObject());
+        }
+        System.gc();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        fillHeap(1000);
+    }
+}
