1bf223375013a04c0a17c1a8b970b8fcd10b0cdb
yqs <411012285@qq.com>
2018-02-08 17:18:33 +0800

    master

--- src/com/lesson8/test/MethodHandleTest.java
+++ src/com/lesson8/test/MethodHandleTest.java
@@ -1,7 +1,27 @@
 package com.lesson8.test;
 
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
 /**
  * Created by yqs on 2018/2/8.
  */
 public class MethodHandleTest {
+    static class ClassA {
+        public void println(String s){
+            System.out.println("51");
+        }
+    }
+
+    private static MethodHandle getPrintLnMH(Object reveiver) throws Throwable {
+        MethodType mt = MethodType.methodType(void.class,String.class);
+        return MethodHandles.lookup().findVirtual(reveiver.getClass(),"println",mt).bindTo(reveiver);
+    }
+
+    public static void main(String[] args) throws Throwable {
+        Object obj = System.currentTimeMillis() %2 == 0?System.out:new ClassA();
+        getPrintLnMH(obj).invokeExact("lesson8");
+    }
 }
