f6cd8b24428e76bf0e503d650c8c336ec4a282ab
progromeryan <411012285@qq.com>
2018-02-08 23:09:12 +0800

    master

---
+++ src/com/lesson8/test/Calc.java
@@ -0,0 +1,14 @@
+package com.lesson8.test;
+
+public class Calc {
+    public static  int calc(){
+        int a = 100;
+        int b = 200;
+        int c= 300;
+        return (a+b)+c;
+    }
+
+    public static void main(String[] args) {
+        calc();
+    }
+}
