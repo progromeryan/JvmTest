0ffe09c1d6e69449432f0f2445faa1c171013725
yqs <411012285@qq.com>
2018-02-08 16:07:00 +0800

    master

---
+++ src/com/lesson8/test/Dispatch.java
@@ -0,0 +1,38 @@
+package com.lesson8.test;
+
+/**
+ * Created by yqs on 2018/2/8.
+ */
+public class Dispatch {
+    static class QQ{}
+
+    static class _360{}
+
+    public static class Father{
+        public void hardChoice(QQ arg){
+            System.out.println("Father choose qq");
+        }
+
+        public void hardChoice(_360 arg){
+            System.out.println("Father choose 360");
+        }
+    }
+
+
+    public static class Son extends Father{
+        public void hardChoice(QQ arg){
+            System.out.println("Son choose qq");
+        }
+
+        public void hardChoice(_360 arg){
+            System.out.println("Son choose 360");
+        }
+    }
+
+    public static void main(String[] args) {
+        Father father = new Father();
+        Father son = new Son();
+        father.hardChoice(new _360());
+        son.hardChoice(new QQ());
+    }
+}
---
+++ src/com/lesson8/test/MethodHandleTest.java
@@ -0,0 +1,7 @@
+package com.lesson8.test;
+
+/**
+ * Created by yqs on 2018/2/8.
+ */
+public class MethodHandleTest {
+}
