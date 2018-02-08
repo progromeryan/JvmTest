12f5da2454bbbc538ce9022ba40c9fdae9f5fdc1
yqs <411012285@qq.com>
2018-02-08 14:29:35 +0800

    master

--- src/com/lesson8/test/DynamicDispatch.java
+++ src/com/lesson8/test/DynamicDispatch.java
@@ -2,6 +2,33 @@ package com.lesson8.test;
 
 /**
  * Created by yqs on 2018/2/8.
+ * 方法动态分配演示
  */
 public class DynamicDispatch {
+    static abstract class Human{
+        protected abstract void sayHello();
+    }
+
+    static class Man extends Human {
+        @Override
+        protected void sayHello() {
+            System.out.println("man say hello");
+        }
+    }
+
+    static class WoMan extends Human {
+        @Override
+        protected void sayHello() {
+            System.out.println("woman say hello");
+        }
+    }
+
+    public static void main(String[] args) {
+        Human man = new Man();
+        Human woman = new WoMan();
+        man.sayHello();
+        woman.sayHello();
+        man = new WoMan();
+        man.sayHello();
+    }
 }
