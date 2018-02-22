e1fbc73c060efe3212cbafe10ec613c0c0d3c399
yqs <411012285@qq.com>
2018-02-22 11:33:42 +0800

    master

--- src/com/lesson12/test/VectorTest.java
+++ src/com/lesson12/test/VectorTest.java
@@ -33,6 +33,8 @@ public class VectorTest {
                         }
                     }
                 }
+
+
             });
             removeThread.start();
             printThread.start();
